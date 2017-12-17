4b8b1bc5528ad3b597fd6ebab203b4ac332e65a4
Wally Yang <wallyyang28@gmail.com>
2017-12-17 14:45:28 +0000

    feat: finish Node class

--- src/red_black_tree.py
+++ src/red_black_tree.py
@@ -1,9 +1,13 @@
 class Node():
-    pass
+    def __init__(self, data):
+        self._data = data
+        self._left = None
+        self._right = None
+        self._is_black = True
 
 class Tree():
-    def __init__(self):
-        pass
+    def __init__(self, data):
+        self._root = Node(data)
 
     def insert(self, data):
         pass
