c2d472f6c3127f3e439b241aae7c09281026be77
Wally Yang <wallyyang28@gmail.com>
2017-12-17 15:33:10 +0000

    feat: revise Node constructor

--- src/red_black_tree.py
+++ src/red_black_tree.py
@@ -1,12 +1,32 @@
 class Node():
-    def __init__(self, data, is_black, left=None, right=None):
+    def __init__(self, data:tuple, is_black, parent=None, left=None, right=None):
+        """Constructor for a Red-Black Tree Node
+
+        Keyword arguments:
+        data -- the data in the node
+        is_black -- color of the node, True for black, False for Red
+        parent -- parent node
+        left -- left subtree
+        right -- right subtree
+        """
         self._data = data
+        self._parent = parent
         self._left = left
         self._right = right
         self._is_black = is_black
 
 class Tree():
+    """A Red Black Tree for indexing
+
+    root -- the root of the tree
+    insert(self, data) -- insert a new data into the tree
+    delete(self, data) -- delete a node given the data
+    find(self, data) -- find a node given the data
+    """
+
+
     def __init__(self):
+        """Constructor for a Red-Black Tree, with an empty tree"""
         self.root = None
 
     def insert(self, data):
