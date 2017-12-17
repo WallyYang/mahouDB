d32a31180201d65bed8b21a3bf12c62559f1f077
Wally Yang <wallyyang28@gmail.com>
2017-12-17 14:12:05 +0000

    feat: red black tree core

---
+++ src/red_black_tree.py
@@ -0,0 +1,15 @@
+class Node():
+    pass
+
+class Tree():
+    def __init__(self):
+        pass
+
+    def insert(self, data):
+        pass
+
+    def delete(self, data):
+        pass
+
+    def find(self, data):
+        pass
