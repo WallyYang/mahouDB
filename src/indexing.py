94d5d86220e7656f9c96ce1332ea2a732221cdaf
Wally Yang <wallyyang28@gmail.com>
2017-12-22 14:02:17 +0000

    docs: add documentation for Index class

--- src/indexing.py
+++ src/indexing.py
@@ -2,15 +2,26 @@ class Index():
     """
     mock class for indexing
 
-    self._index is a multi-map for {column_value - primary_key}
-    primary_key represented by a list containing all the values
+    self._index: a multi-map for {column_value - primary_keys}
+
+    column_value: the value of the column in the table
+    primary_key: list containing all the values
     """
 
     def __init__(self):
+        """initialize an empty index"""
         self._index = dict()
 
 
     def insert(self, pair: tuple):
+        """
+        insert a new pair
+
+        pair: tuple(column_value, new_primary_key)
+        append primary_key if column exists
+        otherwise create a new pair in the dict
+        and initialize primary_key as a empty list
+        """
         if pair[0] not in self._index:
             # new column_value
             self._index[pair[0]] = list()
@@ -19,11 +30,18 @@ class Index():
 
 
     def remove(self, column_value):
+        """
+        remove a pair from the index given a column_value
+        """
         if column_value in self._index:
             del self._index[column_value]
 
 
     def find(self, column_value) -> list:
+        """
+        find the corresponding primary_keys given a column_value
+        return an empty list if not found
+        """
         if column_value in self._index:
             return self._index[column_value]
         else:
