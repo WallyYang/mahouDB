d63598df160c5f80254fe9e0af3792dc4a254700
Wally Yang <wallyyang28@gmail.com>
2017-12-22 13:11:09 +0000

    feat:add mock class Index for tree

---
+++ src/indexing.py
@@ -0,0 +1,13 @@
+class Index():
+
+    def __init__(self):
+        self.index = dict()
+
+    def insert(self, key):
+        pass
+
+    def delete(self, key):
+        pass
+
+    def contains(self, key):
+        pass
