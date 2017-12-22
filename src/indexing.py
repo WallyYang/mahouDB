c71981211878fdc8db38ed944dc3d2ffa5b62dad
Wally Yang <wallyyang28@gmail.com>
2017-12-22 13:50:18 +0000

    feat: finish Index class

--- src/indexing.py
+++ src/indexing.py
@@ -1,13 +1,30 @@
 class Index():
+    """
+    mock class for indexing
+
+    self._index is a multi-map for {column_value - primary_key}
+    primary_key represented by a list containing all the values
+    """
 
     def __init__(self):
-        self.index = dict()
+        self._index = dict()
 
-    def insert(self, key):
-        pass
 
-    def delete(self, key):
-        pass
+    def insert(self, pair: tuple):
+        if pair[0] not in self._index:
+            # new column_value
+            self._index[pair[0]] = list()
 
-    def contains(self, key):
-        pass
+        self._index[pair[0]].append(pair[1])
+
+
+    def remove(self, column_value):
+        if column_value in self._index:
+            del self._index[column_value]
+
+
+    def find(self, column_value) -> list:
+        if column_value in self._index:
+            return self._index[column_value]
+        else:
+            return list()
