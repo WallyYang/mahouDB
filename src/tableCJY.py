0ab2788b71a1d4300ab3baf934a23d095e8778a8
Jimmy Chen <34607251+CJYjimmy@users.noreply.github.com>
2017-12-26 23:21:50 -0500

    Update tableCJY.py

--- src/tableCJY.py
+++ src/tableCJY.py
@@ -1,3 +1,4 @@
+from indexing import Index
 class Table():
     """
     the database table
@@ -11,8 +12,10 @@ class Table():
     def __init__(self, cols: list):
         """initialize an empty table with needed columns"""
         self._cols = cols
-        self._indice = {}
-        self._data = {}
+        self._indices = Index()
+        self._data = Index()
+        for col_name in cols:
+            self._indices.insert((col_name, Index()))
 
     def add(self, row):
         """
@@ -32,64 +35,57 @@ class Table():
                     _row[i] = row[k]
         else:
             raise "row should be instance of list or map"
-        self._data[str(Table.pk_value)] = tuple(_row)
+        self._data.insert((Table.pk_value,tuple(_row)))
+        m = 0
+        for col_name in self._cols:
+            self._indices._index[col_name][0].insert((_row[m],Table.pk_value))
+            m += 1
         Table.pk_value += 1
-        self.update_indice()
 
-    def find(self, col: str, value: str) -> list:
+    def value_find(self, col: str, value: str) -> list:
         """
         find all rows given the column string and value string
         return all corresponding rows appended as a list
         """
-        i = self._cols.index(col)
-        # TODO use index
+        result = self._indices._index[col][0]._index[value]
         ret = []
-        for pk in self._data:
-            r = self._data[pk]
-            if r != None and r[i] == value:
-                ret.append(r)
+        for pk in result:
+            r = self._data.find(pk)
+            ret.append(r)
         return ret
 
-    def remove(self, col:str, value: str):
+    def value_remove(self, col:str, value: str):
         """remove the row(s) which contain the target value,
         and update the each value's index"""
-        i = self._cols.index(col)
-        del_row = []
-        for pk in self._data:
-            r = self._data[pk]
-            if r != None and r[i] == value:
-                del_row.append(pk)
-        for n in range(0,len(del_row)):
-            del self._data[(del_row[n])]
-        self.update_indice()
-
-    def update_indice(self):
-        """update the each value's index which is included in the table"""
-        for col in self._cols:
-            self._indice[col] = {}
-            i = self._cols.index(col)
-            for pk in self._data:
-                r = self._data[pk]
-                if r != None:
-                   val = r[i]
-                   pk_list = [pk]
-                   if not val in self._indice[col]:
-                       self._indice[col][val] = pk_list
-                   else:
-                       self._indice[col][val].append(pk_list)
+        del_row = self._indices._index[col][0]._index[value]
+        self._indices._index[col][0].remove(value)
+        change = {}
+        change_time = 0
+        for pk in del_row:
+            self._data.remove(pk)
+            for c in self._indices._index:
+                for v in self._indices._index[c][0]._index:
+                    for l in range(0,len(self._indices._index[c][0]._index[v])):
+                        if self._indices._index[c][0]._index[v][l] == pk:
+                            change[str(change_time)] = [c,v,str(l)]
+                            change_time += 1
+        for changing in change:
+            del self._indices._index[(change[changing][0])][0]._index[(change[changing][1])][int(change[changing][2])]
+            if len(self._indices._index[(change[changing][0])][0]._index[(change[changing][1])]) == 0:
+                self._indices._index[(change[changing][0])][0].remove(change[changing][1])
 
 if __name__ == "__main__":
     table = Table(["id", "name"])
     table.add(["1", "liu"])
     table.add({"id": "2", "name": "wang"})
     table.add({"id": "2", "name": "wang1"})
+    table.add({"id": "3", "name": "wang"})
     table.add({"id": "4", "name": "jimmy"})
     table.add({"id": "88", "name": "le"})
-    table.remove("name","wang")
-    table.remove("id","88")
+    table.value_remove("id","1")
     table.add({"id": "89","name": "tiger"})
     table.add(["9","chang"])
-    print(table.find("id", "4"))
-    print(table.find("id","9"))
-    for pk in table._data:
-        print(pk)
+    print(table.value_find("id", "4"))
+    print(table.value_find("id","9"))
+    list = table._indices._index["name"][0]._index["wang"]
+    print(list)
